89fadf62afa19222807723634ea58d65bea1a1e8
Soda <mahmoudvv60@gmail.com>
2025-03-25 20:44:53 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,23 @@
 function explodeCat() {
-    let cat = document.getElementById("exploding-cat");
-    cat.src = "explosion1.png";
+    let cat = document.getElementById("exploding-cat"); // Find the cat image
+    let explosionSound = new Audio("explosion.mp3"); // Load the sound
+
+    explosionSound.play(); // Play the explosion sound
+
+    // Stop the sound after 2 seconds
     setTimeout(() => {
-        cat.src = "explosion2.png";
+        explosionSound.pause(); // Pause the sound
+        explosionSound.currentTime = 0; // Reset the sound to the beginning
+    }, 2000);
+
+    cat.src = "explosion1.png"; // Change the image to explosion1
+
+    setTimeout(() => {
+        cat.src = "explosion2.png"; // After 0.5 seconds, change to explosion2
     }, 500);
+
     setTimeout(() => {
-        cat.src = "cat.png";
+        cat.src = "cat.png"; // After 1.5 seconds, change back to normal
     }, 1500);
 }
+
